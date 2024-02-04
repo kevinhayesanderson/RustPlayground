820325dda9ac1b80f14afc5003b81df464ce3e52
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2024-02-04 09:18:31 +0530

    variables fin

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    println!("The value of THREE_HOURS_IN_SECONDS is: {THREE_HOURS_IN_SECONDS}");
+
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+
+    let spaces = "   ";
+    let spaces = spaces.len();
+
+    // let mut spaces = "   ";
+    // spaces = spaces.len(); //error[E0308]: mismatched types
+
+    println!("The value of spaces is: {spaces}");
 }
