f725ea1f25caf1b027be2fd87374a69cbc61f660
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2024-03-10 18:12:46 +0530

    collections and exercises

--- collections/src/hash_maps.rs
+++ collections/src/hash_maps.rs
@@ -1,1 +1,66 @@
-pub fn creating_hash_maps() {}
+use std::collections::HashMap;
+
+pub fn creating_hash_maps() {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+}
+
+pub fn accessing_values() {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+    //The get method returns an Option<&V>;
+    //if there’s no value for that key in the hash map, get will return None.
+    //This program handles the Option by calling copied to get an Option<i32> rather than an Option<&i32>,
+    //then unwrap_or to set score to zero if scores doesn't have an entry for the key.
+    println!("{score}")
+}
+
+pub fn iterating_hash_maps() {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
+}
+
+pub fn updating_hash_maps() {
+    //Overwriting a Value
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Blue"), 25);
+
+    println!("{:?}", scores);
+
+    //Adding a Key and Value Only If a Key Isn’t Present
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}", scores);
+
+    //Updating a Value Based on the Old Value
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
+
+}
--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,8 +1,12 @@
 mod string_collection;
 mod vectors;
+use std::collections::HashMap;
+
 use string_collection as sc;
 mod hash_maps;
 use hash_maps as hm;
+
+use ::function_name::named;
 
 fn main() {
     println!("Hello, world!");
@@ -17,4 +21,85 @@ fn main() {
     sc::iterating_strings();
 
     hm::creating_hash_maps();
+    hm::accessing_values();
+    hm::iterating_hash_maps();
+    hm::updating_hash_maps();
+
+    let (median, mode) = exercise_1(&mut vec![
+        1, 2, 3, 4, 5, 6, 7, 8, 2, 6, 6, 6, 4, 9, 9, 9, 9, 9, 9, 4,
+    ]);
+    println!("Median: {median}");
+    println!("Mode: {mode}");
+
+    let word = String::from("first");
+    println!("Word: {word}");
+    let pig_latin = exercise_2(word);
+    println!("Pig Latin: {pig_latin}");
+    let word = String::from("apple");
+    println!("Word: {word}");
+    let pig_latin = exercise_2(word);
+    println!("Pig Latin: {pig_latin}");
+}
+
+/// exercise_1: median and mode
+// Given a list of integers,
+// use a vector and return the median (when sorted, the value in the middle position)
+// and mode (the value that occurs most often; a hash map will be helpful here) of the list.
+#[named]
+fn exercise_1(values: &mut Vec<i32>) -> (i32, i32) {
+    let function_name = function_name!();
+    println!("Function: {function_name}");
+
+    println!("Values before Sorted: {:?}", values);
+    values.sort();
+    println!("Values after Sorted: {:?}", values);
+
+    let length = values.len();
+    println!("length: {length}");
+    let mid = length / 2;
+    println!("mid: {mid}");
+    let median = values[mid];
+    println!("median: {median}");
+
+    let mut hashmap = HashMap::new();
+    for value in values {
+        let count = hashmap.entry(value).or_insert(1);
+        *count += 1;
+    }
+
+    let mut mode = 0;
+    let mut max_count = 0;
+    for (key, value) in &hashmap {
+        if value > &max_count {
+            max_count = *value;
+            mode = **key;
+        }
+    }
+
+    (median, mode)
+}
+
+/// exercise_2: convert strings to pig latin
+// The first consonant of each word is moved to the end of the word and “ay” is added,
+// so “first” becomes “irst-fay.”
+// Words that start with a vowel have “hay” added to the end instead (“apple” becomes “apple-hay”).
+// Keep in mind the details about UTF-8 encoding!
+#[named]
+fn exercise_2(string: String) -> String {
+    let function_name = function_name!();
+    println!("Function: {function_name}");
+
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+
+    for char in string.chars() {
+        if vowels.contains(&char) {
+            return format!("{string}-hay");
+        } else {
+            let first_consonant = String::from(char);
+            let slice: String = string.chars().into_iter().skip(1).collect();
+            return format!("{slice}-{first_consonant}ay");
+        }
+    }
+
+    return String::new();
 }
